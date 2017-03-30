fb47b63f29ae71a4bdfa818116b8acf44d7d2f4f
MeksPwnschlager <doctoranyk@gmail.com>
2017-03-31 01:03:34 +0200

    improvements mjeheheh

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # randbasel
-A short python script to copmute an aproximation of PI via random numbers
+A short python script to copmute an aproximation of PI via random numbers and the [basel problem](https://plus.maths.org/content/basel-problem).
+It will use the maximum number of iterations it can get, so buckle up!
 
 `python randbasel.py`
-
---
+++ randbasel.py
@@ -0,0 +1,44 @@
+import random
+import fractions
+import math
+import sys
+
+
+max_random = sys.maxsize
+max_iterations = sys.maxsize
+
+def get_random_pair():
+    a, b = random.randint(0, max_random), random.randint(0, max_random)
+    return a, b
+
+def get_pi(count, iter):
+    c = count / iter
+    pi = math.sqrt(6/c)
+    return pi
+
+def approx_pi():
+    count = 0
+    lowest_error = 256
+    best_pi = 0
+
+    for i in range(1, max_iterations):
+        if (fractions.gcd(*get_random_pair()) == 1):
+            count += 1
+        if (count > 0):
+            d = get_pi(count, i)
+            err = d - math.pi
+            print("[" + str(i) + "] Pi / Error: " + str(d) + " / " + str(err))
+            if (err < lowest_error):
+                lowest_error = err
+                best_pi = d
+
+    accuracy = (lowest_error / math.pi) * 100
+    rnd_acc = accuracy
+    print("--------------------------------------------------------------")
+    print("Aproximated PI: " + str(best_pi))
+    print("------ math.pi: " + str(math.pi))
+
+    print("\nAccuracy: " + str(rnd_acc) + "% " + " Lowest Error: " + str(lowest_error))
+    print("--------------------------------------------------------------")
+
+approx_pi()
